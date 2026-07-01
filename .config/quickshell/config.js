d40c8e599c7b6ec988a5e4b352a4ec061490d1d2
gio <gio@cachyos>
2026-07-02 00:44:11 +0300

    Add VSCodium, Helium, Spicetify, Spotify, Obsidian, SDDM, wallpaper, cava, superfile, micro, nwg-bar, quickshell, harlequin, vesktop, discord, systemd configs

---
+++ .config/quickshell/config.js
@@ -0,0 +1,24 @@
+const colors = {
+  bg: "#2e3440",
+  fg: "#d8dee9",
+  muted: "#4c566a",
+  blue: "#61afef",
+  green: "#98c379",
+  yellow: "#e5c07b",
+  orange: "#d19a66",
+  red: "#e06c75",
+  purple: "#c678dd",
+  cyan: "#56b6c2",
+  white: "#abb2bf",
+  black: "#1e1e1e",
+}
+const bar = {
+  fontFamily: "'miracode nerd font mono', monospace",
+  fontSize: "12px",
+  height: "30px",
+}
+
+const notifications = {
+  timeout : 5000,
+}
+
